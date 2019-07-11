ae1f508273f8c8703cafbfdaec33377a94bca913
Paul Unsted <ed__84@hotmail.com>
2019-07-11 09:57:22 -0400

    Plugged in Popper and Tooltip, now I have an on-hover tooltip showing the event's description. Next step is a way for users to dynamically input events.

--- js/app.js
+++ js/app.js
@@ -1,30 +1,39 @@
 // Program waits until everything is loaded before executing any code
 $(document).ready(function () {
 
-    console.log("doc ready")
-
-// FullCalendar (should) load
-    var calendarEl = document.getElementById('calendar');
+// FullCalendar loads
+    var calendarEl = document.getElementById("calendar");
 
     console.log("build calendar")
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
       timeZone: "local",
       plugins: [ "dayGrid" ],
+      
+      eventRender: function(info) {
+        var tooltip = new Tooltip(info.el, {
+          title: info.event.extendedProps.description,
+          placement: "top",
+          trigger: "hover",
+          container: "body"
+        });
+      },
       events: [
         {
           id: "a",
           title: "Event Title",
           start: "2019-07-14T14:00:00",
-          end: "2019-07-14T16:00:00"
+          end: "2019-07-14T16:00:00",
+          description: "a really cool event"
         }
-      ]
+      ],
     });
 
     calendar.render();
 
     var event = calendar.getEventById("a")
     var start = event.start
+    var end = event.end
     console.log(start.toISOString())
 
 });
